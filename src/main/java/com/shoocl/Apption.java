b33ef3116d1334d33af34a6cbc929dc72c4339d4
yu <1430069566@qq.com>
2019-03-09 16:39:43 +0800

    完成

---
+++ src/main/java/com/shoocl/Apption.java
@@ -0,0 +1,12 @@
+package com.shoocl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apption {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apption.class, args);
+	}
+}
