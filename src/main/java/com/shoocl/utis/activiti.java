b060f96cd97b6547c3391ea02c3b7b634fc1afa8
yu <1430069566@qq.com>
2019-03-09 17:43:58 +0800

    qq

---
+++ src/main/java/com/shoocl/utis/activiti.java
@@ -0,0 +1,10 @@
+package com.shoocl.utis;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource(locations={"classpath:activiti.cfg.xml"})
+public class activiti {
+
+}
